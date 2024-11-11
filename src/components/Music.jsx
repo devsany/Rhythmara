4be89e9766e6c1c1bcbc9c2c0a06b77d254f8c2e
devsany <sunnyrajbodhgaya19@gmail.com>
2024-11-11 23:10:36 +0530

    commited to the data

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -5,21 +5,9 @@ import { getDatabase, get, ref, push, set, update } from "firebase/database";
 const Music = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTrack, setCurrentTrack] = useState(0);
-  const audioRef = useRef(null);
   const [song, setSong] = useState([]);
-
-  const tracks = [
-    {
-      title: "Track 1",
-      artist: "Artist 1",
-      url: "https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3", // replace with your track URL
-    },
-    {
-      title: "Track 2",
-      artist: "Artist 2",
-      url: "/path/to/track2.mp3", // replace with your track URL
-    },
-  ];
+  const [audio, setAudio] = useState("");
+  const audioRef = useRef(null);
 
   const togglePlayPause = () => {
     if (isPlaying) {
@@ -31,14 +19,13 @@ const Music = () => {
   };
 
   const handleNextTrack = () => {
-    setCurrentTrack((prevTrack) => (prevTrack + 1) % tracks.length);
+    setCurrentTrack((prevTrack) => (prevTrack + 1) % song.length);
     setIsPlaying(false);
+    setAudio("");
   };
 
   const handlePrevTrack = () => {
-    setCurrentTrack(
-      (prevTrack) => (prevTrack - 1 + tracks.length) % tracks.length
-    );
+    setCurrentTrack((prevTrack) => (prevTrack - 1 + song.length) % song.length);
     setIsPlaying(false);
   };
   const fetchArtist = async () => {
@@ -59,20 +46,34 @@ const Music = () => {
       console.log("Data is not found");
     }
   };
+  const handleAddSong = (id) => {
+    setAudio(id);
+    setIsPlaying(false);
+  };
+  const audio1 = new Audio(audio ? audio : song[currentTrack]?.audioFile);
+  audio1.onloadedmetadata = () => {
+    console.log(audio1.duration); // Set the duration when metadata is loaded
+  };
+
   console.log(song);
   useEffect(() => {
     fetchArtist();
-  }, []);
+  }, [audio, currentTrack]);
   return (
     <div className="music-player">
-      <div className="flex">
+      <div className=" ">
         {song &&
           song.map((item, index) => {
             return (
               <>
-                <div className="">
+                <div
+                  className="border"
+                  onClick={() => {
+                    handleAddSong(item.audioFile);
+                  }}
+                >
                   <div>{item.title}</div>
-                  <div>{item.audioFile} </div>
+                  {/* <div onClick={togglePlayPause}>{item.audioFile} </div> */}
                 </div>
               </>
             );
@@ -80,14 +81,67 @@ const Music = () => {
       </div>
       <audio
         ref={audioRef}
-        src={tracks[currentTrack].url}
+        // src={song[currentTrack]?.audioFile}
+        src={audio ? audio : song[currentTrack]?.audioFile}
         onEnded={handleNextTrack}
       ></audio>
-      <h3>{tracks[currentTrack].title}</h3>
-      <p>{tracks[currentTrack].artist}</p>
-      <button onClick={handlePrevTrack}>Previous</button>
-      <button onClick={togglePlayPause}>{isPlaying ? "Pause" : "Play"}</button>
-      <button onClick={handleNextTrack}>Next</button>
+      <h3>{song[currentTrack]?.title}</h3>
+      <p>{song[currentTrack]?.audioFile}</p>
+      <p>{}</p>
+      Start
+      <div>
+        <div className="fixed bottom-0 left-0 w-full bg-slate-100   text-center p-4">
+          <div>{/* <h3>{song[currentTrack]?.title}</h3> */}</div>
+          <div className="grid grid-cols-11">
+            <div className="col-span-4"></div>
+            <div className="  bg-blue-200 flex rounded-lg items-center text-center justify-center">
+              {" "}
+              <button onClick={handlePrevTrack}>
+                <p className="tracking-tighter font-semibold text-gray-500 md:text-lg dark:text-gray-400">
+                  Previous
+                </p>
+              </button>
+            </div>
+
+            <div>
+              <button onClick={togglePlayPause}>
+                {isPlaying ? (
+                  <>
+                    <div className="flex items-center justify-center">
+                      <img
+                        className="w-12"
+                        src="/song_button/pause-circle-svgrepo-com.png"
+                        alt=""
+                      />
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="flex items-center justify-center  ">
+                      <img
+                        className="w-12"
+                        src="/song_button/play-circle-svgrepo-com.png"
+                        alt=""
+                      />
+                    </div>
+                  </>
+                )}
+              </button>
+            </div>
+            <div>
+              {" "}
+              <div className="  bg-blue-200 flex rounded-lg items-center text-center justify-center">
+                <button onClick={handleNextTrack}>
+                  <p className="tracking-tighter pt-3 pb-3 font-semibold text-gray-500 md:text-lg dark:text-gray-400">
+                    Next
+                  </p>
+                </button>
+              </div>
+            </div>
+            <div className="col-span-4"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
