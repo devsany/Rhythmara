8947360a395b1befae1fc6b01923ede02edfb8f3
devsany <sunnyrajbodhgaya19@gmail.com>
2024-11-28 09:05:40 +0530

    change made and audio tag insert

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,18 @@ function App() {
   return (
     <>
       {/* <UploadSong /> */}
-      <Demo />
+      hii
+      <div className="ml-3">
+        <audio controls>
+          <source
+            src="https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3"
+            // src="https://raw.githubusercontent.com/devsany/mp3-files-hosting/main/Second%20Hand%20Jawaani%20(Full%20Video%20Song)%20Cocktail%20Saif%20Ali%20Khan,%20Deepika%20Padukone%20Pritam.mp3"
+            type="audio/mpeg"
+          />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+      {/* <Demo /> */}
       <Music />
     </>
   );
